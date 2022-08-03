d886de2391b4a7c02d2688b440ef868608081f2c
laurasierra17 <laura.sierra17@gmail.com>
2022-08-03 10:51:41 -0400

    association between User and Post created

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const User = require('./User');
+const Post = require('./Post');
+
+// Establish the associations
+
+// User can have many posts
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+// Post can only have one user
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Project }
