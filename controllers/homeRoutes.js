802f50b8185697c9c5a6782cec9e2803f209a118
laurasierra17 <laura.sierra17@gmail.com>
2022-08-03 21:50:28 -0400

    functionality to get all posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,25 @@
 const router = require('express').Router();
+const { User, Post }= require('../models');
+
+// Gets all posts and JOIN with user data. This will be used on the frontend to display all posts in the Home
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        })
+
+        // Serialize data so the template can read it
+        const posts = postData.map(post => post.get({ plain: true }));
+
+        res.status(200).json(posts)
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const routes = require('./controllers');
 // Handlebars
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 // Sequelize
 const sequelize = require('./config/connection');
 // For authentication
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
 // Set up session
 const sess = {
@@ -32,8 +32,8 @@ const sess = {
 app.use(session(sess));
 
 // Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
