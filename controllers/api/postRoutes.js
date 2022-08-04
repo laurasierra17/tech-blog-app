a62eb3e66c7972d21e68e734e9429cc7e8b99886
laurasierra17 <laura.sierra17@gmail.com>
2022-08-04 00:00:52 -0400

    functionality to store new post in database

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,3 +1,21 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+// Always confirm the user is logged in
+router.use(withAuth);
+
+// Save the user's newly created post
+router.post('/', async (req, res) => {
+    try {
+        // Add the new post to the database
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: req.session.user_id
+        });
+        res.status(200).json(newPost);
+
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
