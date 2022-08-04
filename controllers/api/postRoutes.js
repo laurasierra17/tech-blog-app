b8cf77af0fb28ba9659916675c099f6ce258a52c
laurasierra17 <laura.sierra17@gmail.com>
2022-08-04 00:03:32 -0400

    functionality to delete a post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -19,3 +19,24 @@ router.post('/', async (req, res) => {
         res.status(400).json(err);
     }
 });
+
+// Delete a post
+router.delete('/:id', async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            }
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: "No post found with this id!" });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
